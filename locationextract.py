d601bf89b5a93f09e5c35eba6c00ff4946594322
Viraj Chikhale <chikhaleviraj@gmail.com>
2024-09-17 23:46:35 +0530

    Created a new folder to store the extracted locations info

--- locationextract.py
+++ locationextract.py
@@ -21,9 +21,22 @@ for i in range(0,(no_of_results+1),20):
         'll':'@18.516726,73.856255,10z',
         'start':i
     })
+
+
+    # Use this data for testing
+    # results = {'position': "hiiiiii",
+    #     'title': "hiiiiii",
+    #     'place_id': "hiiiiii",
+    #     'latitude': "hiiiiii",
+    #     'longitude': "hiiiiii",
+    #     'rating': "hiiiiii",
+    #     'reviews': "hiiiiii",
+    #     'type': "hiiiiii",
+    #     'types': "hiiiiii"}
+
     results = str(results)
 
-    with open(f"extracted_data({search})_{i}.json", "w") as outfile:
+    with open(f"extracted_data\{search}_{i}.json", "w") as outfile:
         outfile.write(results)
 
     if(outfile):
